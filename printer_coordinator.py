aba6fe8d33ec5e0a684e13c0ff47b7d343392030
Rohit Macherla <rohit.macherla2@sky.uk>
2017-01-29 19:57:01 +0000

    Adding a poison pill to stop items in queue

--- printer_coordinator.py
+++ printer_coordinator.py
@@ -2,11 +2,16 @@
 import asyncio
 from asyncio import Queue
 
-POISON_PILL = 'POISON_PILL'
+TYPE_POISON_PILL = "Poison Pill"
+PRIORITY_DEFAULT = 5
+PRIORITY_POISON_PILL = 1
 
 
 class PrintManager(object):
     """Fetches data, assigns to printer and does some operations."""
+
+    def __init__(self):
+        self.printer_queue_mapping = {}
 
     def send_to_printer(self, data):
         """Simulate sending data to printer."""
@@ -31,20 +36,25 @@ class PrintManager(object):
             print_item = yield from queue_for_printer.get()
             if hasattr(print_item, "type") and print_item.type is POISON_PILL:
                 break
-            yield from self.send_to_printer(print_item)
-            # TODO: Check status and handle printer error
-            yield from self.update_print_completion(print_item)
+            try:
+                yield from self.send_to_printer(print_item)
+            except PrinterError as e:
+                print("Printer error {}".format(e))
+                self.stop_all_printers()
+                self.start(restart=True)
+            else:
+                yield from self.update_print_completion(print_item)
 
     def get_print_items(self):
         """Dummyd data with printer_id."""
         return [
-            {"name": "a", "printer_id": 1},
-            {"name": "b", "printer_id": 2},
-            {"name": "c", "printer_id": 3},
-            {"name": "d", "printer_id": 4},
-            {"name": "e", "printer_id": 1},
-            {"name": "f", "printer_id": 2},
-            {"name": "g", "printer_id": 3},
+            {"name": "a", "status": STATUS_NOT_STARTED, "printer_id": 1},
+            {"name": "b", "status": STATUS_NOT_STARTED, "printer_id": 2},
+            {"name": "c", "status": STATUS_NOT_STARTED, "printer_id": 3},
+            {"name": "d", "status": STATUS_NOT_STARTED, "printer_id": 4},
+            {"name": "e", "status": STATUS_NOT_STARTED, "printer_id": 1},
+            {"name": "f", "status": STATUS_NOT_STARTED, "printer_id": 2},
+            {"name": "g", "status": STATUS_NOT_STARTED, "printer_id": 3},
         ]
 
     def get_active_printers(self):
@@ -55,19 +65,19 @@ class PrintManager(object):
         """Create a dict with each printer_id as key and data."""
         printer_data_list = {
             "1": [
-                {"name": "a"},
-                {"name": "e"},
+                {"name": "a", "printer_id": 1},
+                {"name": "e", "printer_id": 1},
             ],
             "2": [
-                {"name": "b"},
-                {"name": "f"},
+                {"name": "b", "printer_id": 2},
+                {"name": "f", "printer_id": 2},
             ],
             "3": [
-                {"name": "c"},
-                {"name": "g"},
+                {"name": "c", "printer_id": 3},
+                {"name": "g", "printer_id": 3},
             ],
             "4": [
-                {"name": "d"},
+                {"name": "d", "printer_id": 4},
             ],
         }
         return printer_data_list
@@ -76,15 +86,18 @@ class PrintManager(object):
         """Create a queue for each printer and populate items."""
         queue_printer_tuples = []
         queues = [Queue() for _ in grouped_print_items]
+
         i = 0
         for printer_id, print_item_list in grouped_print_items.items():
-            [queues[i].put_nowait(item) for item in print_item_list]
+            [queues[i].put_nowait((PRIORITY_DEFAULT, item))
+             for item in print_item_list]
             queue_printer_tuples.append((queues[i], printer_id))
+            self.printer_queue_mapping[printer_id] = queues[i]
             i += 1
         print("populate_queues :{}".format(queue_printer_tuples))
         return queue_printer_tuples
 
-    def start(self):
+    def start(self, restart=False):
         """Base algorithm for execution."""
         # 1. Get the list of active printers
         printers = self.get_active_printers()
@@ -105,6 +118,19 @@ class PrintManager(object):
         loop.run_until_complete(asyncio.wait(tasks))
         loop.close()
         return
+
+    def stop_all_printers(self):
+        """Stop all printers from printing the remaining data."""
+        [self.stop_printer(printer_id)
+         for printer_id in self.printer_queue_mapping.itemkeys()]
+
+    def stop_printer(self, printer_id):
+        """Stop the printer with printer_id from printing the remaining."""
+        # TODO: Check if the printer is actually printing something. It may
+        # be idle or in Error state.
+        self.printer_queue_mapping[printer_id].put_nowait(
+            PRIORITY_POISON_PILL, {"type": TYPE_POISON_PILL}
+        )
 
 
 if __name__ == '__main__':
